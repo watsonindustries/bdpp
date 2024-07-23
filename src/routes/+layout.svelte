b27160c4b7c2e011079eedc2960b66825ab8bd92
Daniils Petrovs <daniils@platogo.com>
2024-07-23 22:35:08 +0200

    Add penlight logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,21 @@
 	import '../app.css';
 
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+
+	import PenlightLogo from '$lib/assets/penlight-logo.png?enhanced';
 </script>
 
-<Navbar  >
-  <NavBrand href="/">
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white px-2">Breaking Dimensions PP</span>
-  </NavBrand>
-  <NavHamburger  />
-  <NavUl >
-    <NavLi href="/">Home</NavLi>
-    <NavLi href="/about">About</NavLi>
-    <NavLi href="/contact">Contact</NavLi>
-  </NavUl>
+<Navbar>
+	<NavBrand href="/">
+		<enhanced:img src={PenlightLogo} class="me-3 max-w-20" alt="Penlight Logo 2024" />
+		<!-- <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white px-2">Breaking Dimensions PP</span> -->
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/about">About</NavLi>
+		<NavLi href="/contact">Contact</NavLi>
+	</NavUl>
 </Navbar>
 
 <slot />
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import ServerUrlCopy from 'vite-plugin-url-copy';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-	plugins: [sveltekit(), ServerUrlCopy({ qrcode: { disabled: false } })],
+	plugins: [sveltekit(), ServerUrlCopy({ qrcode: { disabled: false } }), enhancedImages()],
 	server: { host: true },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
