325dc1159bdbdd4d5a123e9d5c9d8ec5999e8943
Daniils Petrovs <daniils@platogo.com>
2024-07-23 22:52:51 +0200

    Add about and contact pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,13 @@
 	import PenlightLogo from '$lib/assets/penlight-logo.png?enhanced';
 </script>
 
-<Navbar>
+<Navbar class="text-primary-500">
 	<NavBrand href="/">
 		<enhanced:img src={PenlightLogo} class="me-3 max-w-20" alt="Penlight Logo 2024" />
 		<!-- <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white px-2">Breaking Dimensions PP</span> -->
 	</NavBrand>
 	<NavHamburger />
-	<NavUl>
+	<NavUl nonActiveClass="text-primary-900 font-semibold">
 		<NavLi href="/">Home</NavLi>
 		<NavLi href="/about">About</NavLi>
 		<NavLi href="/contact">Contact</NavLi>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<div class="p-4">
+	<h1 class="text-4xl font-bold mb-4 tracking-tight bg-gradient-to-br from-primary-600 to-secondary-600 bg-clip-text text-transparent">HoloENLight Breaks Dimensions!</h1>
+	
+	<p class="mb-4 text-primary-900">
+		<span class="font-semibold text-primary-900">HoloENLight</span> is a fan-organized hololive concert penlight project group. Last year we made rainbows at Connect the World, and now we want to bring some colors to Breaking Dimensions!
+	</p>
+	
+	<h2 class="text-2xl tracking-tight font-bold bg-gradient-to-br from-primary-600 to-secondary-600 bg-clip-text text-transparent">How can I participate?</h2>
+	<p class="mb-4 text-primary-900">
+		 During the concerts, when the song <span class="font-semibold italic">Breaking Dimensions</span> starts, change your penlight to the project color, and we’ll create a striking pattern! When the song finishes, the project is over!
+	</p>
+	
+	<p class="mb-4 text-primary-900">
+		To find your project color, read the section and row written on your ticket. Then use this webapp or the map below to find the correct color! Both days will use the same pattern.
+	</p>
+	
+	<div class="mb-4 text-primary-900">
+		<!-- Square space for QR Code near this section -->
+	</div>
+	
+	<!-- Map of the color pattern with sections labeled -->
+	
+	<h2 class="text-2xl tracking-tight font-bold bg-gradient-to-br from-primary-600 to-secondary-600 bg-clip-text text-transparent">How can I help?</h2>
+	<p class="mb-4 text-primary-900">
+		This project can’t succeed without fans spreading the word! Please tell your friends going to Breaking Dimensions about us! Thank you!
+	</p>
+</div>
