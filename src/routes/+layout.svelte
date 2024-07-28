a93333759fc2174e9d58d2228f04cc3be33b8e5f
Daniils Petrovs <daniils@platogo.com>
2024-07-28 22:42:46 +0200

    Improve font styling on desktop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,10 +12,10 @@
 		<!-- <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white px-2">Breaking Dimensions PP</span> -->
 	</NavBrand>
 	<NavHamburger />
-	<NavUl nonActiveClass="text-primary-900 font-semibold">
+	<NavUl nonActiveClass="text-primary-900 font-semibold xl:text-2xl">
 		<NavLi href="/">Home</NavLi>
 		<NavLi href="/about">About</NavLi>
-		<NavLi href="/contact">Contact</NavLi>
+		<!-- <NavLi href="/credits">Credits</NavLi> -->
 	</NavUl>
 </Navbar>
 
