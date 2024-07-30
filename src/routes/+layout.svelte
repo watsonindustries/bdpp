58dcfa4396d2c9cbb8fca021510b1ddc81e00f4c
Daniils Petrovs <daniils@platogo.com>
2024-07-31 01:18:55 +0200

    Improve navbar styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
 	import '../app.css';
 
+	import { page } from '$app/stores';
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+
+	$: activeUrl = $page.url.pathname;
 
 	import PenlightLogo from '$lib/assets/penlight-logo.png?enhanced';
 </script>
@@ -11,10 +14,15 @@
 		<enhanced:img src={PenlightLogo} class="me-3 max-w-20" alt="Penlight Logo 2024" />
 	</NavBrand>
 	<NavHamburger />
-	<NavUl nonActiveClass="text-primary-900 font-semibold xl:text-2xl hover:text-primary-700" activeClass="text-primary-500">
+	<NavUl
+		{activeUrl}
+		nonActiveClass="text-primary-900 font-semibold xl:text-2xl hover:text-primary-700"
+		activeClass="text-primary-500 font-semibold xl:text-2xl"
+	>
 		<NavLi href="/">Home</NavLi>
 		<NavLi href="/about">About</NavLi>
 		<NavLi href="/credits">Credits</NavLi>
+		<NavLi href="/kingblade">KingBlade</NavLi>
 	</NavUl>
 </Navbar>
 
