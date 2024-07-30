58b3d2b4fd2fdf23a2d7c9640fe0c07c52272344
Daniils Petrovs <daniils@platogo.com>
2024-07-31 00:41:45 +0200

    Add credits

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,13 +9,12 @@
 <Navbar class="text-primary-500">
 	<NavBrand href="/">
 		<enhanced:img src={PenlightLogo} class="me-3 max-w-20" alt="Penlight Logo 2024" />
-		<!-- <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white px-2">Breaking Dimensions PP</span> -->
 	</NavBrand>
 	<NavHamburger />
-	<NavUl nonActiveClass="text-primary-900 font-semibold xl:text-2xl">
+	<NavUl nonActiveClass="text-primary-900 font-semibold xl:text-2xl hover:text-primary-700" activeClass="text-primary-500">
 		<NavLi href="/">Home</NavLi>
 		<NavLi href="/about">About</NavLi>
-		<!-- <NavLi href="/credits">Credits</NavLi> -->
+		<NavLi href="/credits">Credits</NavLi>
 	</NavUl>
 </Navbar>
 
--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,7 +1,42 @@
 <script lang="ts">
-    import type { PageData } from '../credits/$types';
-    
-    export let data: PageData;
+	import type { PageData } from '../credits/$types';
+
+	export let data: PageData;
 </script>
 
-Coming Soon!
+<svelte:head>
+	<title>BDPP | Credits</title>
+	<meta name="description" content="Breaking Dimensions Penlight Project credits." />
+</svelte:head>
+
+<section class="mx-auto max-w-2xl space-y-4 p-4">
+	<h1
+		class="bg-gradient-to-br from-primary-600 to-secondary-600 bg-clip-text text-6xl tracking-tight text-transparent"
+	>
+		Credits
+	</h1>
+	<p class="text-xl text-primary-900">
+		HoloENLight would not be possible without the help of the following people:
+	</p>
+
+	<h2 class="text-2xl text-primary-700 xl:text-3xl" id="project-managers">Project Managers</h2>
+	<ul class="list-inside list-disc text-lg text-primary-900 xl:text-xl">
+		<li>Fathendur</li>
+		<li>Iriomote</li>
+		<li>Ninjastahr</li>
+	</ul>
+
+	<h2 class="text-2xl text-primary-700 xl:text-3xl" id="web-development">Web Development</h2>
+	<ul class="list-inside list-disc text-lg text-primary-900 xl:text-xl">
+		<li>danirukun</li>
+		<li>Moondog</li>
+		Special thanks to GoldElysium for logistics support.
+	</ul>
+
+	<h2 class="text-2xl text-primary-700 xl:text-3xl" id="graphic-design">Graphic Design</h2>
+	<ul class="list-inside list-disc text-lg text-primary-900 xl:text-xl">
+		<li>Skiddosaur</li>
+	</ul>
+
+	<p class="text-lg text-primary-900">...and many other fans!</p>
+</section>
